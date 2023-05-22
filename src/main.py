0e7feb7f77eb1d09e80080babd73915777e2d6d3
Patrick Smadbeck <patsmad@gmail.com>
2023-05-22 10:29:42 -0400

    Add api key check to api calls

--- src/main.py
+++ src/main.py
@@ -7,20 +7,23 @@ app = Flask(__name__)
 
 api = api_builder.build()
 
-# TODO: Add in wrapper to check api key on each call automatically
 @app.route('/files/')
+@config.api_check
 def files() -> list[dict]:
    return [file.to_dict() for file in api.get_all_files()]
 
 @app.route('/items/')
+@config.api_check
 def items() -> list[dict]:
    return [item.to_dict() for item in api.get_all_items()]
 
 @app.route('/boxes/')
+@config.api_check
 def boxes() -> list[dict]:
    return [box.to_dict() for box in api.get_all_boxes()]
 
 @app.route('/links/')
+@config.api_check
 def links() -> list[dict]:
    return [link.to_dict() for link in api.get_all_links()]
 
--- src/util/config.py
+++ src/util/config.py
@@ -1,9 +1,35 @@
 from .io import readJSON
+from flask import request
+from functools import wraps
+from typing import Optional, Callable
 
 class Config:
-    def __init__(self):
+    def __init__(self) -> None:
         config: dict = readJSON('config')
         self.api_key: str = config['api-key']
 
     def api_key_match(self, api_key: str) -> bool:
         return api_key == self.api_key
+
+    def api_check(self, fnc: Callable) -> Callable:
+        @wraps(fnc)
+        def inner_api_check(*args, **kwargs):
+            maybe_api_key: Optional[str] = request.args.get('api_key')
+            if maybe_api_key is None:
+                raise APIMissingException
+            elif not self.api_key_match(maybe_api_key):
+                raise APIInvalidException
+            else:
+                return fnc(*args, **kwargs)
+        return inner_api_check
+
+class APIMissingException(Exception):
+
+    def __init__(self) -> None:
+        super().__init__("Must provide valid api_key as ?api_key=<key> on request")
+
+class APIInvalidException(Exception):
+    "Must provide valid api_key as ?api_key=<key> on request"
+
+    def __init__(self) -> None:
+        super().__init__("Invalid API Key")
